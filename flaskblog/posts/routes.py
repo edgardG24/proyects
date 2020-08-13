a45fbab270c9642e47ca9d7982b500d50e422ca5
edgardG24 <69501502+edgardG24@users.noreply.github.com>
2020-08-12 23:43:51 -0600

    Update routes.py

--- flaskblog/posts/routes.py
+++ flaskblog/posts/routes.py
@@ -4,6 +4,7 @@ from flask_login import current_user, login_required
 from flaskblog import db
 from flaskblog.models import Post
 from flaskblog.posts.forms import PostForm
+import psycopg2
 
 posts = Blueprint('posts', __name__)
 
@@ -14,6 +15,9 @@ def new_post():
     form = PostForm()
     if form.validate_on_submit():
         post = Post(title=form.title.data, content=form.content.data, author=current_user)
+        cur = conn.cursor()
+        cur.execute("INSERT INTO post (id, title, date_posted, content, user_id) VALUES (7, 'Tesst', '2020-08-08 21:08:27.494782', 'First Post content!', 1);")
+        conn.commit()
         db.session.add(post)
         db.session.commit()
         flash('¡Tu post ha sido creado!', 'success')
@@ -27,6 +31,13 @@ def post(post_id):
     post = Post.query.get_or_404(post_id)
     return render_template('post.html', title=post.title, post=post)
 
+conn = psycopg2.connect(
+	database="d3qgi53uaj1knr",
+	user="tyiuqokgzyrbem",
+	password="7a906139c3b520dab15e6f603d3ff0c40f7b88d313d1db6cd4f603a04961c0b3",
+	host="ec2-34-195-115-225.compute-1.amazonaws.com",
+	port="5432"
+)
 
 @posts.route("/post/<int:post_id>/update", methods=['GET', 'POST'])
 @login_required
